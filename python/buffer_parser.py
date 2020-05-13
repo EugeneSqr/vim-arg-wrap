bdd8b01447d0d8894d548044f926d9b867f004f4
eugenesqr <eskurihin@gmail.com>
2020-05-14 00:09:30 +0300

    methods for finding first opening and last closing brackets in a range

--- python/buffer_parser.py
+++ python/buffer_parser.py
@@ -44,3 +44,44 @@ def _get_line_indent(line):
 
 def _parse_args_string(args):
     return list(map(str.strip, args.split(',')))
+
+def _get_last_closing_bracket_index(cursor, buffer):
+    row_index, _ = _cursor_to_index(cursor)
+    for current_row_index in range(row_index, len(buffer)):
+        line = buffer[current_row_index]
+        last_bracket_col_index = line.rfind(')', 0, len(line))
+        if last_bracket_col_index != -1:
+            if last_bracket_col_index == len(line) - 1:
+                return current_row_index, last_bracket_col_index
+            ending_col_index = _skip_spaces_after_last_closing_bracket(line, last_bracket_col_index)
+            if ending_col_index == len(line) or line[ending_col_index] != ',':
+                return current_row_index, last_bracket_col_index
+    return None
+
+def _skip_spaces_after_last_closing_bracket(line, index):
+    ending_index = index + 1
+    while ending_index < len(line) and line[ending_index] == ' ':
+        ending_index += 1
+    return ending_index
+
+def _get_first_opening_bracket_index(last_closing_bracket_index, buffer):
+    if last_closing_bracket_index is None:
+        return None
+    row_index, col_index = last_closing_bracket_index
+    bracket_balance = 0
+    for current_row_index in range(row_index, -1, -1):
+        line = buffer[current_row_index]
+        if current_row_index != row_index:
+            col_index = len(line) - 1
+        for current_col_index in range(col_index, -1, -1):
+            bracket_balance = _update_bracket_balance(line[current_col_index], bracket_balance)
+            if bracket_balance == 0:
+                return current_row_index, current_col_index
+    return None
+
+def _update_bracket_balance(current_char, bracket_balance):
+    if current_char == '(':
+        bracket_balance -= 1
+    elif current_char == ')':
+        bracket_balance += 1
+    return bracket_balance
