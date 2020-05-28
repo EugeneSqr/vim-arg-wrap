a8486afac448dea582dba2b83163b154e69ddf41
eugenesqr <eskurihin@gmail.com>
2020-05-28 13:55:36 +0300

    nowrap implemented

--- python/wrappers/wrapper_nowrap.py
+++ python/wrappers/wrapper_nowrap.py
@@ -6,8 +6,8 @@ class ArgWrapperNoWrap(ArgWrapperBase):
     Converts block of text to its nowrapped form:
     invoke_method(a, b, c)
     """
-
     def _wrap_args(self, parsed_range, buffer):
-        print('Wrap Args from nowrap')
-        # TODO: implement me
-        # pass
+        buffer[parsed_range.start_row_index] = (parsed_range.beginning +
+                                                ', '.join(parsed_range.args) +
+                                                parsed_range.ending)
+        del buffer[parsed_range.start_row_index + 1:parsed_range.end_row_index + 1]
