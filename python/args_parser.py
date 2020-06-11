1a5339c19c9a099ad0160a476d0851061e500682
eugenesqr <eskurihin@gmail.com>
2020-06-11 14:09:45 +0300

    advanced arg parsing implemented

--- python/args_parser.py
+++ python/args_parser.py
@@ -1,2 +1,51 @@
 def parse_args_line(args_line):
-    return list(map(str.strip, args_line.split(',')))
+    args = []
+    current_arg = ''
+    brackets_balance = _BracketsBalance([('(', ')'), ('[', ']'), ('{', '}')])
+
+    for char in args_line:
+        if char == ',':
+            if brackets_balance.is_balanced():
+                _append_arg(args, current_arg)
+                current_arg = ''
+            else:
+                current_arg += char
+        else:
+            current_arg += char
+            if not brackets_balance.is_valid(char):
+                break
+    _append_arg(args, current_arg)
+    return args if args else [args_line]
+
+def _append_arg(args, current_arg=''):
+    current_arg = current_arg.strip()
+    if current_arg:
+        args.append(current_arg)
+
+class _BracketsBalance():
+    def __init__(self, bracket_pairs):
+        self._balances_by_bracket_type = list(map(_BracketTypeBalance, bracket_pairs))
+
+    def is_balanced(self):
+        return all(map(lambda _: _.get() == 0, self._balances_by_bracket_type))
+
+    def is_valid(self, char):
+        def bracket_type_balance_valid(bracket_type_balance):
+            bracket_type_balance.update(char)
+            return bracket_type_balance.get() >= 0
+
+        return all(map(bracket_type_balance_valid, self._balances_by_bracket_type))
+
+class _BracketTypeBalance():
+    def __init__(self, bracket_pair):
+        self._opening_bracket, self._closing_bracket = bracket_pair
+        self._balance = 0
+
+    def get(self):
+        return self._balance
+
+    def update(self, char):
+        if char == self._opening_bracket:
+            self._balance += 1
+        elif char == self._closing_bracket:
+            self._balance -= 1
---
+++ python/args_parser_test.py
@@ -0,0 +1,22 @@
+import pytest
+
+import args_parser
+
+def test_parse_args_line_empty_args_returns_array_with_original_args_line():
+    assert args_parser.parse_args_line(',,,') == [',,,']
+
+
+@pytest.mark.parametrize('invalid_line', [')', ']', '}', 'a(}', '[)', '(()]'])
+def test_parse_args_line_invalid_args_returns_array_with_original_args_line(invalid_line):
+    assert args_parser.parse_args_line(invalid_line) == [invalid_line]
+
+def test_parse_args_line_returns_args_with_spaces_discarded():
+    assert args_parser.parse_args_line('  a  , b,c  , d') == ['a', 'b', 'c', 'd']
+
+def test_parse_args_line_nested_calls_single_bracket_type_returns_args():
+    assert (args_parser.parse_args_line('l11(l21(l31(a, b), c), l22(d, e)), f') ==
+            ['l11(l21(l31(a, b), c), l22(d, e))', 'f'])
+
+def test_parse_args_line_nested_calls_multiple_bracket_types_returns_args():
+    assert (args_parser.parse_args_line('l1([1,2,3,], {"a": [3,6,7],}), l2[5], {}, []') ==
+            ['l1([1,2,3,], {"a": [3,6,7],})', 'l2[5]', '{}', '[]'])
