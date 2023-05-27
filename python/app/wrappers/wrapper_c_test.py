39a888bddbc6eff4923199b5c1c9f0115f7e44d3
eugenesqr <eskurihin@gmail.com>
2023-05-27 09:41:45 +0300

    add annotation for nowrap tests

--- python/app/wrappers/wrapper_c_test.py
+++ python/app/wrappers/wrapper_c_test.py
@@ -1,10 +1,13 @@
+from typing import Tuple
+from unittest.mock import Mock
+
 import pytest
 
 from app.buffer_parser import Signature, RowsRange
 from app.conftest import VimBufferMock, assert_buffer
 from . import wrapper_c
 
-def test_c_wrap_args_single_line(mock_signature_at_cursor):
+def test_c_wrap_args_single_line(mock_signature_at_cursor: Mock) -> None:
     buffer = VimBufferMock([
         ' # c begin comment',
         '   def c_func():',
@@ -22,7 +25,7 @@ def test_c_wrap_args_single_line(mock_signature_at_cursor):
         ' # c end comment',
     ])
 
-def test_c_wrap_args_two_lines(mock_signature_at_cursor):
+def test_c_wrap_args_two_lines(mock_signature_at_cursor: Mock) -> None:
     buffer = VimBufferMock([
         ' # c begin comment',
         '    def c_func():',
@@ -31,7 +34,7 @@ def test_c_wrap_args_two_lines(mock_signature_at_cursor):
         ' # c end comment',
     ])
     mock_signature_at_cursor(_build_signature(RowsRange(2, 3, 8)))
-    wrapper_c.ArgWrapperC(None).wrap_args((4, 0), buffer)
+    wrapper_c.ArgWrapperC(0).wrap_args((4, 0), buffer)
     assert_buffer(buffer, [
         ' # c begin comment',
         '    def c_func():',
@@ -41,7 +44,7 @@ def test_c_wrap_args_two_lines(mock_signature_at_cursor):
         ' # c end comment',
     ])
 
-def test_c_wrap_args_multiple_lines(mock_signature_at_cursor):
+def test_c_wrap_args_multiple_lines(mock_signature_at_cursor: Mock) -> None:
     buffer = VimBufferMock([
         ' # c begin comment',
         '    def c_func():',
@@ -52,7 +55,7 @@ def test_c_wrap_args_multiple_lines(mock_signature_at_cursor):
         ' # c end comment',
     ])
     mock_signature_at_cursor(_build_signature(RowsRange(2, 5, 8)))
-    wrapper_c.ArgWrapperC(None).wrap_args((6, 0), buffer)
+    wrapper_c.ArgWrapperC(0).wrap_args((6, 0), buffer)
     assert_buffer(buffer, [
         ' # c begin comment',
         '    def c_func():',
@@ -62,7 +65,7 @@ def test_c_wrap_args_multiple_lines(mock_signature_at_cursor):
         ' # c end comment',
     ])
 
-def test_c_wrap_args_multiple_lines_below_first(mock_signature_at_cursor):
+def test_c_wrap_args_multiple_lines_below_first(mock_signature_at_cursor: Mock) -> None:
     buffer = VimBufferMock([
         ' # c begin comment',
         '    def c_func():',
@@ -72,7 +75,7 @@ def test_c_wrap_args_multiple_lines_below_first(mock_signature_at_cursor):
         ' # c end comment',
     ])
     mock_signature_at_cursor(_build_signature(RowsRange(2, 4, 8)))
-    wrapper_c.ArgWrapperC(None).wrap_args((5, 0), buffer)
+    wrapper_c.ArgWrapperC(0).wrap_args((5, 0), buffer)
     assert_buffer(buffer, [
         ' # c begin comment',
         '    def c_func():',
@@ -82,24 +85,27 @@ def test_c_wrap_args_multiple_lines_below_first(mock_signature_at_cursor):
         ' # c end comment',
     ])
 
-def test_c_recognizes_c(mock_signature_at_cursor):
+def test_c_recognizes_c(mock_signature_at_cursor: Mock) -> None:
     mock_signature_at_cursor(_build_signature(RowsRange(2, 4, 0)))
-    assert wrapper_c.ArgWrapperC(4).recognized(None, None) is True
+    assert wrapper_c.ArgWrapperC(4).recognized((0, 0), VimBufferMock([])) is True
 
-def test_c_does_not_recognize_empty_range(mock_signature_at_cursor):
+def test_c_does_not_recognize_empty_range(mock_signature_at_cursor: Mock) -> None:
     mock_signature_at_cursor(Signature())
-    assert wrapper_c.ArgWrapperC(4).recognized(None, None) is False
+    assert wrapper_c.ArgWrapperC(4).recognized((0, 0), VimBufferMock([])) is False
 
-def test_c_does_not_recognize_empty_args(mock_signature_at_cursor):
-    mock_signature_at_cursor(_build_signature(RowsRange(2, 4, 0), args=[]))
-    assert wrapper_c.ArgWrapperC(4).recognized(None, None) is False
+def test_c_does_not_recognize_empty_args(mock_signature_at_cursor: Mock) -> None:
+    mock_signature_at_cursor(_build_signature(RowsRange(2, 4, 0), args=()))
+    assert wrapper_c.ArgWrapperC(4).recognized((0, 0), VimBufferMock([])) is False
 
 @pytest.mark.parametrize('c_row_index_diff', [0, 1, 3, 200])
-def test_c_does_not_recognize_other_ranges(mock_signature_at_cursor, c_row_index_diff):
+def test_c_does_not_recognize_other_ranges(mock_signature_at_cursor: Mock,
+                                           c_row_index_diff: int) -> None:
     start_row = 5
     rows_range = RowsRange(start_row, start_row + c_row_index_diff, 0)
     mock_signature_at_cursor(_build_signature(rows_range))
-    assert wrapper_c.ArgWrapperC(2).recognized(None, None) is False
+    assert wrapper_c.ArgWrapperC(2).recognized((0, 0), VimBufferMock([])) is False
 
-def _build_signature(rows_range, args=('c_a', 'c_b', 'c_c'), ending=')'):
+def _build_signature(rows_range: RowsRange,
+                     args: Tuple[str, ...]=('c_a', 'c_b', 'c_c'),
+                     ending: str=')') -> Signature:
     return Signature(rows=rows_range, beginning='        c_method(', args=args, ending=ending)
