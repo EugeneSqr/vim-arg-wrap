5851806a3e189b57e36ea3bef8c9d7866c54b891
eugenesqr <eskurihin@gmail.com>
2023-05-28 18:22:27 +0300

    adding type annotations completed

--- python/app/wrappers/wrapper_sequence_test.py
+++ python/app/wrappers/wrapper_sequence_test.py
@@ -1,7 +1,10 @@
 from unittest.mock import Mock
+from typing import cast
 
 from app.wrappers import wrapper_sequence
+from app.wrappers.wrapper_base import ArgWrapperBase
 from app.conftest import VimBufferMock
+from app.types import Cursor
 
 def test_first_next_to_second() -> None:
     wrapper_mocks = [
@@ -13,7 +16,7 @@ def test_first_next_to_second() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_next(cursor, buffer)
-    wrapper_mocks[1].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[1], cursor, buffer)
 
 def test_second_next_to_thrird() -> None:
     wrapper_mocks = [
@@ -25,7 +28,7 @@ def test_second_next_to_thrird() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_next(cursor, buffer)
-    wrapper_mocks[2].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[2], cursor, buffer)
 
 def test_last_next_to_first() -> None:
     wrapper_mocks = [
@@ -37,7 +40,7 @@ def test_last_next_to_first() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_next(cursor, buffer)
-    wrapper_mocks[0].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[0], cursor, buffer)
 
 def test_last_back_to_last_but_one() -> None:
     wrapper_mocks = [
@@ -49,7 +52,7 @@ def test_last_back_to_last_but_one() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_prev(cursor, buffer)
-    wrapper_mocks[2].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[2], cursor, buffer)
 
 def test_first_back_to_last() -> None:
     wrapper_mocks = [
@@ -61,7 +64,7 @@ def test_first_back_to_last() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_prev(cursor, buffer)
-    wrapper_mocks[-1].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[-1], cursor, buffer)
 
 def test_third_back_to_second() -> None:
     wrapper_mocks = [
@@ -73,7 +76,7 @@ def test_third_back_to_second() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_prev(cursor, buffer)
-    wrapper_mocks[1].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[1], cursor, buffer)
 
 def test_first_next_to_second_multiple_recognized() -> None:
     wrapper_mocks = [
@@ -85,7 +88,7 @@ def test_first_next_to_second_multiple_recognized() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_next(cursor, buffer)
-    wrapper_mocks[1].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[1], cursor, buffer)
 
 def test_first_back_to_last_multiple_recognized() -> None:
     wrapper_mocks = [
@@ -97,10 +100,12 @@ def test_first_back_to_last_multiple_recognized() -> None:
     sequence = wrapper_sequence.WrapperSequence(wrapper_mocks)
     cursor, buffer = (0, 0), VimBufferMock([])
     sequence.wrap_prev(cursor, buffer)
-    wrapper_mocks[-1].wrap_args.assert_called_once_with(cursor, buffer)
+    _assert_wrap_args_called_once_with(wrapper_mocks[-1], cursor, buffer)
 
-def _create_wrapper(should_recognize: bool):
-    return type('', (), {
-        'recognized': Mock(return_value=should_recognize),
-        'wrap_args': Mock(),
-    })
+def _create_wrapper(should_recognize: bool) -> ArgWrapperBase:
+    return Mock(spec=ArgWrapperBase, recognized=Mock(return_value=should_recognize))
+
+def _assert_wrap_args_called_once_with(wrapper: ArgWrapperBase,
+                                       cursor: Cursor,
+                                       buffer: VimBufferMock) -> None:
+    cast(Mock, wrapper.wrap_args).assert_called_once_with(cursor, buffer)
